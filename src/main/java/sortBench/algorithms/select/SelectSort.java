1be1dc117358853bd84e350e862bb5fd40b5cb6f
Dijkstra <contiemathias@gmail.com>
2019-06-17 21:57:13 +0200

    Add of the package on the top and move the java file to the right place

---
+++ src/main/java/sortBench/algorithms/select/SelectSort.java
@@ -0,0 +1,25 @@
+package sortBench.algorithm
+
+public class SelectSort {
+	
+ 	 
+	    public static void doSelectionSort(int[] arr){
+	         
+	        for (int i = 0; i < arr.length - 1; i++)
+	        {
+	            int index = i;
+	            for (int j = i + 1; j < arr.length; j++)
+	                if (arr[j] < arr[index]) 
+	                    index = j;
+	      
+	            int smallerNumber = arr[index];  
+	            arr[index] = arr[i];
+	            arr[i] = smallerNumber;
+	        }
+	        
+	    }
+	     
+	    
+	    
+	}
+
