9d6725d7fc4e80a892499a27af4884d2f97b9741
pampletousse <sloppin23@gmail.com>
2019-06-17 11:29:06 +0200

    Deplacement du tri a bulles dans un fichier dédié nommé BubbleSort.java

---
+++ src/main/java/sortBench/algorithms/BubbleSort.java
@@ -0,0 +1,13 @@
+// bubble sort method
+    public static void bubbleSort(int array[]){
+        int n = array.length;
+        int k;
+        for (int m = n; m >= 0; m--) {
+            for (int i = 0; i < n - 1; i++) {
+                k = i + 1;
+                if (array[i] > array[k]) {
+                    swapNumbers(i, k, array);
+                }
+            }
+        }
+    }
